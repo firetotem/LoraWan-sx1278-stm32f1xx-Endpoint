a07caea537050b9c18a93ffa85d5d2fb9b224b7f
fpv <fedorov_p88@mail.ru>
2019-04-15 17:59:37 +0300

    Added some code

--- Utils/inc/sw_rtc.h
+++ Utils/inc/sw_rtc.h
@@ -9,23 +9,27 @@
 /* Тип времени */
 typedef struct
 {
-	uint32_t SubSecond;
-	uint32_t Seconds;
-	uint32_t Minutes;
-	uint32_t Hours;
+
+  uint32_t SubSecond;
+  uint32_t Seconds;
+  uint32_t Minutes;
+  uint32_t Hours;
+
 } SW_RTC_TimeTypeDef_t;
 
 /* Тип будильника */
 typedef struct
 {
-	SW_RTC_TimeTypeDef_t AlarmTime;
-	uint16_t 			 AlarmIsActive;
+
+  SW_RTC_TimeTypeDef_t AlarmTime;
+  uint16_t 	       AlarmIsActive;
+
 } SW_RTC_AlarmTypeDef_t;
 
 /* Exported macros */
 #define RTC_START_TIME_SECOND	0UL
 #define RTC_START_TIME_MINUTE	0UL
-#define RTC_START_TIME_HOUR		0UL
+#define RTC_START_TIME_HOUR	8UL
 
 #define TICKS_IN_SECOND	1000UL
 #define TICKS_IN_MINUTE	TICKS_IN_SECOND*60
@@ -40,12 +44,25 @@ void SW_RTC_Init( void );
 void SW_RTC_IrqHandler( void );
 
 /*
+ * Get time context;
+ */
+uint32_t SW_RTC_GetTimeContext( void );
+
+/*
+ * Get Calendar value in ticks;
+ */
+uint32_t SW_RTC_SetTimeContext( void );
+
+
+
+
+
+
+
+/*
  * Alarm Irq handler;
  */
 void SW_RTC_ALarm_IrqHandler( void );
-
-
-void SW_RTC_SetTime( SW_RTC_TimeTypeDef_t *sTime );
 
 void SW_RTC_SetAlarm( uint32_t timeout );
 
--- Utils/src/sw_rtc.c
+++ Utils/src/sw_rtc.c
@@ -2,15 +2,24 @@
 
 typedef struct
 {
-	uint32_t TickTime;
-	SW_RTC_TimeTypeDef_t RTC_Calndr_Time;
+
+  uint32_t             TickTime;
+  SW_RTC_TimeTypeDef_t RTC_Calndr_Time;
 
 } SW_RTC_TimeContextTypeDef_t;
 
-static SW_RTC_AlarmTypeDef_t SwAlarm = { 0 };
+
 
 /* Переменная для хранения времени */
 static SW_RTC_TimeContextTypeDef_t SwTimeContext = { 0 };
+
+/* Register of sw ticks */
+static volatile uint32_t SW_TICK_REG = 0;
+
+/* Alarm */
+static SW_RTC_AlarmTypeDef_t SwAlarm = { 0 };
+
+
 
 /*
  * Установка времени. Устанавливается время и потом пересчитывается
@@ -19,16 +28,25 @@ static SW_RTC_TimeContextTypeDef_t SwTimeContext = { 0 };
 static void SW_RTC_SetTime(SW_RTC_TimeTypeDef_t *sTime);
 
 
+/* Rtc irq handler */
+void SW_RTC_IrqHandler( void )
+{
+  SW_TICK_REG++;
+}
+
+
 /* Инициализация контекста времени; */
 void SW_RTC_Init( void )
 {
-	SW_RTC_TimeTypeDef_t timeStruct;
 
-	timeStruct.Seconds = RTC_START_TIME_SECOND;
-	timeStruct.Minutes = RTC_START_TIME_MINUTE;
-	timeStruct.Hours   = RTC_START_TIME_HOUR;
+  SW_RTC_TimeTypeDef_t timeStruct;
 
-	SW_RTC_SetTime( &timeStruct );
+  timeStruct.Seconds = RTC_START_TIME_SECOND;
+  timeStruct.Minutes = RTC_START_TIME_MINUTE;
+  timeStruct.Hours   = RTC_START_TIME_HOUR;
+
+  SW_RTC_SetTime( &timeStruct );
+
 }
 
 
@@ -37,25 +55,53 @@ void SW_RTC_Init( void )
  */
 static void SW_RTC_SetTime(SW_RTC_TimeTypeDef_t *sTime)
 {
-	uint32_t tickNum = 0;
+  uint32_t tickNum = 0;
 
-	SwTimeContext.RTC_Calndr_Time = *sTime;
+  SwTimeContext.RTC_Calndr_Time = *sTime;
 
-	tickNum += sTime->SubSecond;
-	tickNum += sTime->Seconds 	* 1000;
-	tickNum += sTime->Minutes 	* 1000 * 60;
-	tickNum += sTime->Hours 	* 1000 * 60 * 60;
+  tickNum += sTime->SubSecond;
+  tickNum += sTime->Seconds 	* 1000;
+  tickNum += sTime->Minutes 	* 1000 * 60;
+  tickNum += sTime->Hours 	* 1000 * 60 * 60;
 
-	SwTimeContext.TickTime = tickNum;
+  SwTimeContext.TickTime = tickNum;
+  SW_TICK_REG = tickNum;
 }
+
+
+/*
+ * Get time context;
+ */
+uint32_t SW_RTC_GetTimeContext( void )
+{
+  return SwTimeContext.TickTime;
+}
+
+
+/*
+ * Get Calendar value in ticks;
+ */
+uint32_t SW_RTC_SetTimeContext( void )
+{
+  SwTimeContext.TickTime = SW_RTC_GetCalndrValue( &SwTimeContext );
+  return SwTimeContext.TickTime;
+}
+
+
+static uint32_t SW_RTC_GetTime( SW_RTC_TimeTypeDef_t *sTime )
+{
+
+}
+
 
 
 void SW_RTC_SetAlarm( uint32_t timeout )
 {
-	uint32_t rtcAlarmSubseconds = 0;
-	uint32_t rtcAlarmSeconds    = 0;
-	uint32_t rtcAlarmMinutes    = 0;
-	uint32_t rtcAlarmHour		= 0;
+
+  uint32_t rtcAlarmSubseconds = 0;
+  uint32_t rtcAlarmSeconds    = 0;
+  uint32_t rtcAlarmMinutes    = 0;
+  uint32_t rtcAlarmHour	      = 0;
 
 
 	rtcAlarmHour = SwTimeContext.RTC_Calndr_Time.Hours;
@@ -74,9 +120,5 @@ void SW_RTC_SetAlarm( uint32_t timeout )
 
 
 
-/* Обработчик прерывания */
-void SW_RTC_IrqHandler( void )
-{
-	SwTimeContext.TickTime++;
-}
+
 
